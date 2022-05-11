543de5e48f5e47ca6e31c0fc5529f33d61252086
15minutOdmora <liammislej@gmail.com>
2022-05-11 20:03:16 +0200

    Posodobljeno po 7. predavanjih.

--- code/file_actions/conversions.py
+++ code/file_actions/conversions.py
@@ -5,15 +5,29 @@ Module for converting different types of data.
 
 def dollars_to_euro(dollar_value: str):
     # $1 = 0.95€
-    pass
+    value = int(dollar_value[1:])
+    euro_value = value * 0.95
+    return f"{round(euro_value)}€"
 
 
 def lbs_to_kg(lbs_value: str):
     # 1lbs = 0.45359237kg
-    pass
+    value = lbs_value[:-3]
+    kg = round(int(value) * 0.45359237)
+    return f"{kg}kg"
 
 
 def imperial_to_metric(imperial_value: str):
     # 1' = 0.3048m
     # 1" = 0.0254m
-    pass
+    feet, inches = imperial_value.split("\'")
+    feet, inches = int(feet), int(inches[:-1])
+    meters = round(feet * 0.3048 + inches * 0.0254, 2)
+    return f"{meters}m"
+
+
+if __name__ == "__main__":
+    # Tests
+    print(dollars_to_euro("$456231"))
+    print(lbs_to_kg("100lbs"))
+    print(imperial_to_metric('6\'10'))
--- code/file_actions/main.py
+++ code/file_actions/main.py
@@ -2,46 +2,30 @@
 Module for the main functionality of converting data from one file type to others.
 """
 
-import os
-
 from file_handling import *
 import conversions
 
 
 DATA_PATH = "code/file_actions/data/"
 
-def join_path(path1, path2):
-    return os.path.join(path1, path2)
-
-
-def get_from_txt_file():
-    """
-    Grabs header and all rows from set txt file.
-    """
-    rows = read_txt(join_path(DATA_PATH, "dallas_players.txt"))
-    header = rows[0][:-1].split(" ")
-    data = []
-    for row in rows[1:]:
-        data.append(row[:-1].split(" "))
-    return header, data
 
 def main():
-    """
-    Convert txt file to .csv and .json
-    """
-    header, data = get_from_txt_file()
-
-    write_csv(join_path(DATA_PATH, "dallas_players.csv"), header, data)
-
-    players = []
-    for row in data:
-        print(row)
-        player = {}
-        for i, column in enumerate(header):
-            player[column] = row[i]
-        players.append(player)
     
-    write_json(join_path(DATA_PATH, "dallas_players.json"), players)
+    full_path = DATA_PATH + "dallas_players.txt"
+
+    # Read and clean lines
+    data = []
+    for line in read_txt(full_path):
+        clean_line = line[:-1]
+        line_data = clean_line.split(" ")
+        data.append(line_data)
+
+    # Convert data
+    for i in range(1, len(data)):
+        data[i][-1] = conversions.dollars_to_euro(data[i][-1])
+        data[i][-3] = conversions.lbs_to_kg(data[i][-3])
+        data[i][-4] = conversions.imperial_to_metric(data[i][-4])
+        print(data[i])
 
 
 if __name__ == "__main__":
